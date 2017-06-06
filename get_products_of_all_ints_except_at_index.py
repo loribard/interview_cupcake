f828453d0c9814b4266e61ffa8a2426c38febad8
Lori Bard <lori.r.bard@gmail.com>
2017-06-06 09:14:21 -0700

    improving

--- get_products_of_all_ints_except_at_index.py
+++ get_products_of_all_ints_except_at_index.py
@@ -1,12 +1,14 @@
 
 def get_products_of_all_ints_except_at_index(list_of_ints):
     product_list = []
-    if list_of_ints < 2:
-        return []
+    if len(list_of_ints) < 2:
+        return 'Getting the product of numbers at other indices\nrequires at least two numbers. You input %s .' % str(list_of_ints)
+    list_lower = 1
     for i in range(len(list_of_ints)):
-        list_lower = list_of_ints[:i]
+        if i >= 1:
+            list_lower *= list_of_ints[i-1]
         list_upper = list_of_ints[i+1:]
-        first = product(list_lower)
+        first = list_lower
         second = product(list_upper)
         product_list.append(first*second)
     return product_list
@@ -23,7 +25,8 @@ def product(lista):
 lista = [1, 7, 3, 4]
 listb = [1]
 listc = [1, 3]
-listd = [2, 6, 8, 10]
+listd = [2, 6, 8, 0]
+
 print get_products_of_all_ints_except_at_index(lista)
 print get_products_of_all_ints_except_at_index(listb)
 print get_products_of_all_ints_except_at_index(listc)
